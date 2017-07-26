dbd028a16e16cf93d44e2d1c43392a0b8c511986
Mistyazur <mistyazur@gmail.com>
2017-07-26 14:10:46 +0800

    Initial commit

---
+++ qqrencode.h
@@ -0,0 +1,48 @@
+#ifndef QQRENCODE_H
+#define QQRENCODE_H
+
+#include <QtCore>
+
+class QQREncodePrivate;
+
+class QQREncode
+{
+    Q_GADGET
+    Q_ENUMS(ErrorCorrectionLevel)
+
+public:
+    enum ErrorCorrectionLevel {
+        LOW,
+        MEDIUM,
+        QUARTILE,
+        HIGH
+    };
+
+    QQREncode();
+    ~QQREncode();
+
+    void setLevel(ErrorCorrectionLevel value);
+    ErrorCorrectionLevel getLevel() const;
+    void setVersion(int version);
+    int version() const;
+    void setMargin(int value);
+    int margin() const;
+    void setMicro(bool value);
+    bool isMicro() const;
+    void setBackground(QColor color);
+    void setForeground(QColor color);
+
+    bool encode(QByteArray input);
+    bool encode(QString input, bool caseSensitive=true);
+    bool encodeKanji(QByteArray input, bool caseSensitive=true);
+
+    bool toSVG(QString output, int size);
+    QImage toQImage(int size=0);
+
+private:
+    Q_DISABLE_COPY(QQREncode)
+    QScopedPointer<QQREncodePrivate> d_ptr;
+    Q_DECLARE_PRIVATE(QQREncode)
+};
+
+#endif // QQRENCODE_H
